81c7921b4ddfc5ee7671ce57652531ce2ce93af0
Jennyads <jennycristina.1998@gmail.com>
2024-05-27 14:31:24 -0300

    added score for answer

--- projeto_perguntas/lib/questao.dart
+++ projeto_perguntas/lib/questao.dart
@@ -1,18 +1,18 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Questao extends StatelessWidget {
   final String texto;
-  Questao(this.texto);
+
+  const Questao(this.texto, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       margin: const EdgeInsets.all(10),
-      child: Text(texto,
-          style: const TextStyle(
-            fontSize: 28
-          ),
+      child: Text(
+        texto,
+        style: const TextStyle(fontSize: 28),
         textAlign: TextAlign.center,
       ),
     );
